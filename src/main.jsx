33f89435fd5a1b09fb93c28661a55e74a51304c7
Mariusz Trocewicz <throcu@gmail.com>
2023-07-13 23:38:55 +0200

    add: first commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         loading={null}
         persistor={persistor}
       >
-        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+        <BrowserRouter basename='/deploy-test-new/'>
           <ChakraProvider>
             <App />
           </ChakraProvider>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-08-phonebook',
+  base: '/deploy-test-new/',
 });
