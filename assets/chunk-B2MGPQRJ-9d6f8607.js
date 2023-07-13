78512809cc73719597c0503b569ecd0f99f92d79
Mariusz Trocewicz <throcu@gmail.com>
2023-07-13 23:40:11 +0200

    Updates

---
+++ assets/chunk-B2MGPQRJ-9d6f8607.js
@@ -0,0 +1,1 @@
+import{f as n,i as t,o as r,j as c,a as o,d as m}from"./index-80b615b6.js";var d=n(function(a,s){const e=t("Heading",a),{className:f,...i}=r(a);return c.jsx(o.h2,{ref:s,className:m("chakra-heading",a.className),...i,__css:e})});d.displayName="Heading";export{d as H};
